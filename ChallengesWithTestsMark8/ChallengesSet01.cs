be58b4807d14ab876f39a2b36c906770fd9a637c
russelibale808 <russelibale808@gmail.com>
2022-11-03 22:30:44 -1000

    WeeklyChallenges01

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,43 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return (minuend - subtrahend);
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int result = number2;
+
+            if (number1 < number2)
+            {
+                result= number1;
+            }
+            return result;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            return $"Hello, {nameOfPerson}!";
+
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return ("HEY!");
         }
     }
 }
